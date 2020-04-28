dd9a7b53761485fc21c75dd798b1eb636731008d
bengyles <bengyles@hotmail.com>
2020-04-29 00:20:43 +0200

    Updated web3, changed server and read through internal transactions

--- trace.js
+++ trace.js
@@ -4,31 +4,29 @@ const trace = require('./transactionTrace');
 
 const Web3 = require('web3');
 const web3 = new Web3();
-web3.setProvider(new web3.providers.HttpProvider('http://104.154.88.76:8545'));
+web3.setProvider(new web3.providers.HttpProvider('http://35.221.208.98:8545'));
 
 //const filter = web3.eth.filter("latest")
 //filter.watch(function (error, blockHash) {
-web3.eth.getBlock('0xa728b5495f55f3cdba5ee5f09cdf195726c4e77541ca35232935b221d50c3601').then(function(block) {
+web3.eth.getBlock('0x69189bcefe487544baab63800ad372920fa8099bf8487ea3d16d6d034a828da1').then(async function(block) {
   console.log('Block #' + block.number);
-  _(block.transactions).each(txHash => {
-    console.log(txHash); 
-    web3.eth.getTransaction(txHash).then((tx) => {
-     // web3.eth.getTransactionReceipt(txHash).then((tr) => {    // this one seems not returning well, promised?
-        console.log(tx.value);
-       // console.log(`${tr.transactionIndex}: ${tx.from} -> ${tx.to} (${tx.value} wei, ${tr.cumulativeGasUsed} gas, ${tr.logs.length} event(s))`);
-        //if (tr.contractAddress) {
-        //  console.log('   Contract created at ' + tr.contractAddress);
-        //}
+  await _(block.transactions).each(async function(txHash) {
+    console.log(txHash);
+    let tx = await web3.eth.getTransaction(txHash).catch((e) => console.log('an error occured while getting the transaction'));
+    let tr = await web3.eth.getTransactionReceipt(txHash);  // this one seems not returning well, promised?
+    console.log(tx.value);
+    console.log(`${tr.transactionIndex}: ${tx.from} -> ${tx.to} (${tx.value} wei, ${tr.cumulativeGasUsed} gas, ${tr.logs.length} event(s))`);
+    //if (tr.contractAddress) {
+    //  console.log('   Contract created at ' + tr.contractAddress);
+    //}
 
-        trace.getInternalTransfers(web3.currentProvider, txHash, tx.to).then((tfs) => {
-            //console.log (tfs); 
-	    /* _(tfs).each(tf => {
-                console.log(`  ${tf.senderAddress} -> ${tf.recipientAddress} (${tf.etherSent} wei)`);
-              });*/
-            },
-        );
-
-      //}).catch((e) => {console.log(e);});
-    }).catch((e) => console.log('an error occured while getting the transaction'));
+    let tfs = await trace.getInternalTransfers(web3.currentProvider, txHash, tx.to);
+    console.log(tfs);
+    _(tfs).each(tf => {
+      console.log(`  ${tf.senderAddress} -> ${tf.recipientAddress} (${tf.etherSent} wei)`);
+    });
   });
+}).catch(e => {
+  console.log('an error occured:');
+  console.log(e);
 });
--- transactionTrace.js
+++ transactionTrace.js
@@ -1,22 +1,16 @@
 const _ = require('lodash');
 
 async function getRawTrace(provider, txHash) {
-  return await provider.send({
-    method: 'debug_traceTransaction',
-    params: [txHash, {}],
-    jsonrpc: '2.0',
-    id: '1',
-  }, await function(err, result) {
-    // if (err) {
-    //   console.log(err);
-    //   return [];
-    // }
-    console.log(result);
+  return new Promise((resolve, reject) => {
+    provider.send({
+      method: 'debug_traceTransaction',
+      params: [txHash, {}],
+      jsonrpc: '2.0',
+      id: '1',
+    }, (err, res) => {
+      return err ? reject(err) : resolve(res.result);
+    });
   });
-  // if (!trace) {
-  //   console.error('Trace not received from RPC server, please start geth with --rpcapi "eth,net,web3,debug" and without --fast');
-  //   return [];
-  // }
 }
 
 /** get internal transactions: CALL transfers
@@ -24,69 +18,59 @@ async function getRawTrace(provider, txHash) {
  * TODO: CREATE, CALLCODE, DELEGATECALL, SUICIDE
  */
 async function getInternalTransfers(provider, txHash, txTo) {
-  await provider.send({
-    method: 'debug_traceTransaction',
-    params: [txHash, {}],
-    jsonrpc: '2.0',
-    id: '1',
-  }, await function(err, result) {
-    // if (err) {
-    //   console.log(err);
-    //   return [];
-    // }
-    const trace = result.result;
-    //console.log(trace);   /// it's correct already, TLDR;
 
-    const stack = [{address: txTo}];
-    const transfers = [];
-    _(trace.structLogs).forEach(step => {
-      if (step.error) {
-        stack.pop();
-        return;  // continue
-      }
+  const trace = await getRawTrace(provider, txHash);
+  //console.log(trace);   /// it's correct already, TLDR;
 
-      // just returned
-      if (step.depth === stack.length - 1) {
-        // fix up CREATE dummy address
-      }
-	
-      switch (step.op) {
-        case 'CALL': {
+  const stack = [{address: txTo}];
+  const transfers = [];
+  _(trace.structLogs).forEach(step => {
+    if (step.error) {
+      stack.pop();
+      return;  // continue
+    }
 
-          const etherSent = parseInt(step.stack[step.stack.length - 3], 16);
-          const recipientAddress = '0x' + step.stack[step.stack.length - 2].slice(-40);
+    // just returned
+    if (step.depth === stack.length - 1) {
+      // fix up CREATE dummy address
+    }
 
-          if (etherSent > 0) {
-            transfers.push({
-              etherSent,
-              recipientAddress,
-              senderAddress: stack[stack.length - 1].address,
-              gasUsed: step.gasCost,
-              //eventCount:  TODO: count sent events?
-            });
-          }
-          stack.push({address: recipientAddress});
-	  //console.log (transfers);
-          break;
+    switch (step.op) {
+      case 'CALL': {
+
+        const etherSent = parseInt(step.stack[step.stack.length - 3], 16);
+        const recipientAddress = '0x' + step.stack[step.stack.length - 2].slice(-40);
+
+        if (etherSent > 0) {
+          transfers.push({
+            etherSent,
+            recipientAddress,
+            senderAddress: stack[stack.length - 1].address,
+            gasUsed: step.gasCost,
+            //eventCount:  TODO: count sent events?
+          });
         }
-        case 'CREATE': {
-          // TODO: report transfer after address is known
-          // const etherSent = parseInt(step.stack[step.stack.length - 1], 16)
-          stack.push({address: 'unknown'});
-          break;
-        }
-        case 'CALLCODE':
-        case 'DELEGATECALL':
-          stack.push({address: step.stack[step.stack.length - 1]});
-          break;
-        case 'SUICIDE':
-          // TODO: report transfer
-          break;
+        stack.push({address: recipientAddress});
+        //console.log (transfers);
+        break;
       }
-    });
-    console.log(transfers);
-    return transfers;
+      case 'CREATE': {
+        // TODO: report transfer after address is known
+        // const etherSent = parseInt(step.stack[step.stack.length - 1], 16)
+        stack.push({address: 'unknown'});
+        break;
+      }
+      case 'CALLCODE':
+      case 'DELEGATECALL':
+        stack.push({address: step.stack[step.stack.length - 1]});
+        break;
+      case 'SUICIDE':
+        // TODO: report transfer
+        break;
+    }
   });
+  // console.log(transfers);
+  return transfers;
 }
 
 module.exports = {getInternalTransfers, getRawTrace};
