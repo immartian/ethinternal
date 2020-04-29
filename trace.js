b61a6cd1caf5163f2e8b6480df3908a862e6cbe1
im <isaac@musicoin.org>
2020-04-29 10:29:13 +0800

    add some more probing blocks

--- trace.js
+++ trace.js
@@ -4,11 +4,14 @@ const trace = require('./transactionTrace');
 
 const Web3 = require('web3');
 const web3 = new Web3();
-web3.setProvider(new web3.providers.HttpProvider('http://35.221.208.98:8545'));
 
-//const filter = web3.eth.filter("latest")
-//filter.watch(function (error, blockHash) {
-web3.eth.getBlock('0x69189bcefe487544baab63800ad372920fa8099bf8487ea3d16d6d034a828da1').then(async function(block) {
+web3.setProvider(new web3.providers.HttpProvider('http://35.221.208.98:8545'));  
+// first: 35.221.208.98
+//another endpoint:   35.200.37.219
+
+
+function  getInternalsFromBlock(bHash){
+web3.eth.getBlock(bHash).then(async function(block) {
   console.log('Block #' + block.number);
   for (let i = 0; i < block.transactions.length; i++) {
     let txHash = block.transactions[i];
@@ -31,3 +34,22 @@ web3.eth.getBlock('0x69189bcefe487544baab63800ad372920fa8099bf8487ea3d16d6d034a8
   console.log('an error occured:');
   console.log(e);
 });
+}
+
+
+
+blockHash= "0x936b109ad813df99ae7b57c98c0019987eca8b986d2cd85b368620f561999532" // (#2031)
+getInternalsFromBlock(blockHash); 
+
+blockHash = "0xbce3f81ce2db539901e1c527369c1d3e928c3f58ebf817db42e90a64e366bb23" // (#10930)
+getInternalsFromBlock(blockHash); 
+
+blockHash = "0x7077f0d7d9bf245c2656c8fcc62c27da27dc35e48b38a6b1fcb0e12c43a7f2e3" // (#2000003) 
+getInternalsFromBlock(blockHash); 
+
+blockHash= "0x07733e630839aa40b54bda914423cbcf288c5953c2ea84234056dfbe5e669db6"  //5,383,837
+getInternalsFromBlock(blockHash); 
+
+blockHash = "0x69189bcefe487544baab63800ad372920fa8099bf8487ea3d16d6d034a828da1"  // 6,193,845
+getInternalsFromBlock(blockHash); 
+
