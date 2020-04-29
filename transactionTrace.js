c3f63a28aa8de86d6eddba90c143a8bd99652519
bengyles <bengyles@hotmail.com>
2020-04-29 12:51:38 +0200

    Added Async to get the different blocks in order, added a check on structLogs to avoid an error when there are none

--- transactionTrace.js
+++ transactionTrace.js
@@ -24,51 +24,53 @@ async function getInternalTransfers(provider, txHash, txTo) {
 
   const stack = [{address: txTo}];
   const transfers = [];
-  _(trace.structLogs).forEach(step => {
-    if (step.error) {
-      stack.pop();
-      return;  // continue
-    }
+  if (trace && trace.structLogs) {
+    _(trace.structLogs).forEach(step => {
+      if (step.error) {
+        stack.pop();
+        return;  // continue
+      }
 
-    // just returned
-    if (step.depth === stack.length - 1) {
-      // fix up CREATE dummy address
-    }
+      // just returned
+      if (step.depth === stack.length - 1) {
+        // fix up CREATE dummy address
+      }
 
-    switch (step.op) {
-      case 'CALL': {
+      switch (step.op) {
+        case 'CALL': {
 
-        const etherSent = parseInt(step.stack[step.stack.length - 3], 16);
-        const recipientAddress = '0x' + step.stack[step.stack.length - 2].slice(-40);
+          const etherSent = parseInt(step.stack[step.stack.length - 3], 16);
+          const recipientAddress = '0x' + step.stack[step.stack.length - 2].slice(-40);
 
-        if (etherSent > 0) {
-          transfers.push({
-            etherSent,
-            recipientAddress,
-            senderAddress: stack[stack.length - 1].address,
-            gasUsed: step.gasCost,
-            //eventCount:  TODO: count sent events?
-          });
+          if (etherSent > 0) {
+            transfers.push({
+              etherSent,
+              recipientAddress,
+              senderAddress: stack[stack.length - 1].address,
+              gasUsed: step.gasCost,
+              //eventCount:  TODO: count sent events?
+            });
+          }
+          stack.push({address: recipientAddress});
+          //console.log (transfers);
+          break;
         }
-        stack.push({address: recipientAddress});
-        //console.log (transfers);
-        break;
+        case 'CREATE': {
+          // TODO: report transfer after address is known
+          // const etherSent = parseInt(step.stack[step.stack.length - 1], 16)
+          stack.push({address: 'unknown'});
+          break;
+        }
+        case 'CALLCODE':
+        case 'DELEGATECALL':
+          stack.push({address: step.stack[step.stack.length - 1]});
+          break;
+        case 'SUICIDE':
+          // TODO: report transfer
+          break;
       }
-      case 'CREATE': {
-        // TODO: report transfer after address is known
-        // const etherSent = parseInt(step.stack[step.stack.length - 1], 16)
-        stack.push({address: 'unknown'});
-        break;
-      }
-      case 'CALLCODE':
-      case 'DELEGATECALL':
-        stack.push({address: step.stack[step.stack.length - 1]});
-        break;
-      case 'SUICIDE':
-        // TODO: report transfer
-        break;
-    }
-  });
+    });
+  }
   // console.log(transfers);
   return transfers;
 }
